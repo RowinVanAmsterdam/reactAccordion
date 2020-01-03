56ffcb0ccfada67d7eea4d7108f4ccbade4b78b0
Rowin van Amsterdam <rowin.amsterdam@gmail.com>
2020-01-03 16:59:12 +0100

    Fixed some lint errors, added links in json text

--- src/components/UI/Accordion/Accordion.tsx
+++ src/components/UI/Accordion/Accordion.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { CategoryCollapse } from './CategoryCollapse';
 import FAQData from '../../../static/FAQ-Data.json';
-// import FAQDataTsx from '../../../static/FAQ-Data-TSX';
 
 const Accordion = () => {
-    // const dataFile = FAQDataTsx();
 
     return (
         <div className="container container--xtiny">
--- src/components/UI/Accordion/SingleCollapse.tsx
+++ src/components/UI/Accordion/SingleCollapse.tsx
@@ -4,28 +4,26 @@ import Icon from '../Shared/Icon';
 type props = {
     question: string;
     answer: string;
-}
+};
 
 const SingleCollapse = ({question, answer}: props) => {
-
 
     const [active, setActive] = React.useState('notActive');
     const [isTrue, setTrue] = React.useState(false);
 
-
     const toggleAccordion = () => {
         setActive(active === 'notActive' ? 'active' : 'notActive');
-        setTrue(!isTrue)
+        setTrue(!isTrue);
     };
 
     return (
-        <li onClick={toggleAccordion} className='question' aria-expanded={isTrue}>
+        <li onClick={toggleAccordion} className="question" aria-expanded={isTrue}>
             <h4 onClick={toggleAccordion} aria-expanded={isTrue}>
                 {question}
-                <Icon name="arrow-right-thin" className='icon__svg' />
+                <Icon name="arrow-right-thin" className="icon__svg" />
             </h4>
             <div className={`answer ${active}`}>
-                <p className='answer__body' dangerouslySetInnerHTML={{__html: answer}}/>
+                <p className="answer__body" dangerouslySetInnerHTML={{__html: answer}}/>
             </div>
         </li>
     );
